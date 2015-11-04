0adbb067ec6ddbda5f026285d8b00f77580c609e
amay22 <amay@nyu.edu>
2015-11-04 18:02:58 -0500

    Added a Django Project with default settings and templates

---
+++ manage.py
@@ -0,0 +1,11 @@
+"""manage.py module of django project"""
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyFolio.settings")
+
+  from django.core.management import execute_from_command_line
+
+  execute_from_command_line(sys.argv)
