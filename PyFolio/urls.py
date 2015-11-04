0b8d1eccfe003b862e4eb67bbbca0297e3c81358
amay22 <amay@nyu.edu>
2015-11-04 18:40:29 -0500

    Added Login Urls to get the login app working

--- PyFolio/urls.py
+++ PyFolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from LoginFolio import urls as LoginFolio_urls
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include(LoginFolio_urls))
 ]
