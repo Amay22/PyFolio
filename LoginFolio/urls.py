56b7d7d04903d2e6cb9d18916277b46d54a5cf35
amay22 <amay@nyu.edu>
2015-12-01 01:41:32 -0500

    Changed to single quotes

--- LoginFolio/urls.py
+++ LoginFolio/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from . import views as LoginFolio_views
 
 urlpatterns = [
-    url(r'^$', LoginFolio_views.login_user, name="login"),
-    url(r'^(?i)login', LoginFolio_views.login_user, name="login"),
-    url(r'^(?i)home', LoginFolio_views.home, name="home"),
-    url(r'^(?i)about', LoginFolio_views.about, name="about"),
-    url(r'^(?i)team', LoginFolio_views.team, name="team"),
-    url(r'^(?i)logout', LoginFolio_views.logout_user, name="logout"),
-    url(r'^(?i)register', LoginFolio_views.register_user, name="register")
+    url(r'^$', LoginFolio_views.home, name='home'),
+    url(r'^(?i)login', LoginFolio_views.login_user, name='login'),
+    url(r'^(?i)home', LoginFolio_views.home, name='home'),
+    url(r'^(?i)about', LoginFolio_views.about, name='about'),
+    url(r'^(?i)team', LoginFolio_views.team, name='team'),
+    url(r'^(?i)logout', LoginFolio_views.logout_user, name='logout'),
+    url(r'^(?i)register', LoginFolio_views.register_user, name='register')
 ]
